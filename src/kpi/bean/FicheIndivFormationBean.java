e8961884635b3a660a0abf54c86d832253bdad36
nbehlouli <nbehlouli>
2019-02-07 19:00:37 +0100

    modif fiche individuelle+correction typo formation externe

---
+++ src/kpi/bean/FicheIndivFormationBean.java
@@ -0,0 +1,72 @@
+package kpi.bean;
+
+public class FicheIndivFormationBean {
+
+
+	 private String libelleechelle;
+	 private String domaine_formation;
+	 private String thematique_domaine_formation;
+	 private String action_formation;
+	 private String proposee;
+	 private String validee;
+	 private String programmee;
+	 private String realisee;
+	
+	 
+	 	 
+
+	public String getLibelleechelle() {
+		return libelleechelle;
+	}
+	public void setLibelleechelle(String libelleechelle) {
+		this.libelleechelle = libelleechelle;
+	}
+	
+	
+	public String getProposee() {
+		return proposee;
+	}
+	public void setProposee(String proposee) {
+		this.proposee = proposee;
+	}
+	public String getValidee() {
+		return validee;
+	}
+	public void setValidee(String validee) {
+		this.validee = validee;
+	}
+	public String getProgrammee() {
+		return programmee;
+	}
+	public void setProgrammee(String programmee) {
+		this.programmee = programmee;
+	}
+	public String getRealisee() {
+		return realisee;
+	}
+	public void setRealisee(String realisee) {
+		this.realisee = realisee;
+	}
+	public String getDomaine_formation() {
+		return domaine_formation;
+	}
+	public void setDomaine_formation(String domaine_formation) {
+		this.domaine_formation = domaine_formation;
+	}
+	public String getThematique_domaine_formation() {
+		return thematique_domaine_formation;
+	}
+	public void setThematique_domaine_formation(String thematique_domaine_formation) {
+		this.thematique_domaine_formation = thematique_domaine_formation;
+	}
+	public String getAction_formation() {
+		return action_formation;
+	}
+	public void setAction_formation(String action_formation) {
+		this.action_formation = action_formation;
+	}
+	
+
+	 
+	 
+}
